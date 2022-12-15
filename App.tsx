ea37e8bbd716b3e4d533a291c1177b95238d5a60
Rakesh Arunachalam <rakesh@thoughtbot.com>
2022-12-15 17:00:12 +0000

    Update App to use Stack navigator
    - Added Stack Navigator to the App with a MapScreen stack.
    - Modified App to use MapScreen
    - Updated snapshots
    - Added a sleep function in the tests as per
      https://github.com/react-navigation/react-navigation/issues/10943

--- App.tsx
+++ App.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MapView from 'react-native-maps';
 import {SafeAreaView, StyleSheet} from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+const MapScreen = () => (
+  <SafeAreaView style={styles.container}>
+    <MapView testID="mapView" style={styles.mapContainer} />
+  </SafeAreaView>
+);
 
 const App = () => {
   return (
     <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-        <MapView testID="mapView" style={styles.mapContainer} />
-      </SafeAreaView>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Map"
+          component={MapScreen}
+          options={{
+            title: 'Humon',
+            headerStyle: {
+              backgroundColor: 'rgb(0, 179, 153)',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
